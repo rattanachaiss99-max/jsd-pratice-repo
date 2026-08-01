b8b1dfbea622bccfa14d20ec8cc39cc44b6b4687
Rattanachai Kanjiak <rattanachaiss99@gmail.com>
2026-08-01 10:21:41 +0700

    delete old files

--- shared/js/main.js
+++
@@ -1,7 +0,0 @@
-/**
- * Go Thailand Shared JavaScript Helper Functions
- */
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('🚀 Go Thailand Monorepo - Shared JS Loaded Successfully!');
-});
